ebc1ffeca2a23871d21ce4d94634f73cbcacc6e4
Nicolás Alvarez <52534795+nicolas-alv@users.noreply.github.com>
2019-08-02 12:18:53 -0300

    2/8/19

---
+++ ej1.js
@@ -0,0 +1,56 @@
+function ej1(){
+
+  var lado1=prompt("Ingrese lado uno");
+  var lado2=prompt("Ingrese lado dos");
+  var angulo;
+  lado1=parseInt(lado1);
+  lado2=parseInt(lado2);
+  angulo=parseInt(angulo);
+
+  while(angulo==90){
+
+  }
+
+
+
+  }
+
+  function ej2(){ //Datos de tenistas
+
+    var nombreTenista;
+    var acumuladorNombre=0;
+    var edadTenista;
+    var acumuladorEdad=0;
+    var victotiasTenista;
+    var acumuladorPG=0;
+    var derrotasTenista;
+    var acumuladorPP=0;
+    var interacción;
+    var contador=0;
+
+    while(true){
+      interacción=confirm("Ingrese datos de tenista");
+      if (interacción=null){
+        break;
+      }
+      else{
+        nombreTenista=prompt("Ingrese nombre del tenista");
+      }
+
+      edadTenista=prompt("Ingrese la edad del tenista");
+      edadTenista=parseInt(edadTenista);
+      acumuladorEdad=edadTenista + acumuladorEdad;
+    }
+      victoriasTenista=parseInt(victoriasTenista);
+
+
+
+
+
+
+    }
+
+
+
+
+
