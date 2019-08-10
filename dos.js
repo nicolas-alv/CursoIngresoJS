d6f31ff4448e7132a4b0a973c398db42c170b8cd
Nicolás Alvarez <52534795+nicolas-alv@users.noreply.github.com>
2019-08-10 13:03:38 -0300

    intento de resolucion del 2

--- dos.js
+++ dos.js
@@ -1,19 +1,64 @@
 function mostrar()
-{
-  var mascota;
-  var contAnimal;
-  var edad;
-  var peso;
-  var promPeso;
+/*{
   var nombre;
-  var acumNombre;
+  var peso;
+  var tipo;
+  var edad;
+  var acumulador=0;
+  var contador=0;
+
+  while(contador <4){
+    contador++;
+  }
 
   do {
-      mascota=prompt("Ingrese elnombre de su mascota");
 
-    }while(isNaN(mascota))
+    tipo=prompt("ingrese un Tipo");
+
+  }while (tipo != "perro" || tipo != "gato");
+  {
+    tipo=prompt("Error, Ingrese un tipo valido");
+  }
+  
+  do {
+       peso=prompt("ingrese un peso");
+    
+      }while (isNaN(peso) || peso <1 || peso >100);
+      {
+        tipo=prompt("Error, Ingrese un tipo valido");
+      
+      }
+      
+
+    
+
+}*/
+{
+  
+    var nombre;
+    var peso;
+    var tipo;
+    var edad;
+    var acumulador=0;
+    var contador=0;
+
+  while (contador < 4)
+    contador++;
+
+    tipo=prompt("Ingrese un tipo de animal");
+    while (tipo != "perro" && tipo !="Gato")
+    {
+      tipo=prompt("Incorrecto, ingrese un tipo valido");
+    }
+
+    peso=prompt("Ingrese el peso");
+    while ( peso < 1 || peso>100 )
+    {
+      peso=prompt("El peso es incorrecto, ingrese un valor entre 1 y 100");
+
+    }
 
 
-
-
+  
+  
 }
